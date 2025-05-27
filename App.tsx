1f65c33b70f65b2035497ffaef4ef0cd4cff3696
Sohail30-rgb <syed-sohail.mohiuddin@capgemini.com>
2025-05-27 10:57:06 +0530

    till setup of @react-native-firebase/app

--- App.tsx
+++ App.tsx
@@ -1,22 +1,47 @@
-
-
-import React from 'react';
+import React,{useState,useEffect} from 'react';
  import {
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
-  View,
+  View
 } from 'react-native';
+import { getAuth, onAuthStateChanged } from '@react-native-firebase/auth';
 
- 
- 
-function App(): React.JSX.Element {
+  
+function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<{email: string | null} | null>(null);
+
+  function handleAuthStateChanged(user: {email: string | null} | null) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+   useEffect(() => {
+    const subscriber = onAuthStateChanged(getAuth(), (user) => {
+      if (user) {
+        setUser({ email: user.email });
+      } else {
+        setUser(null);
+      }
+      if (initializing) setInitializing(false);
+    });
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+   if (initializing) return null;
+    if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
    return (
-    <View style={{ flex: 1, backgroundColor: '#F5FCFF' }}>
-       <Text>cdcdc</Text>
-       <Text>jhjk,n</Text>
+    <View style={{ flex: 1, backgroundColor: 'red',justifyContent: 'center', alignItems: 'center',padding:22 }}>
+       <Text>Welcome {user.email ?? "User"}</Text>
     </View>
   );
 }
